92726e0a0b9defb5d4cd5344c91e159a4f087e3a
YuriMDP <yuriubatuba2019@gmail.com>
2024-05-10 23:42:13 -0300

    Criação dos models

---
+++ src/controllers/SessionController.js
@@ -0,0 +1,16 @@
+/*
+  index (listALL): listagem de sessões
+  store (add): criar uma sessão
+  show (list): listar uma UNICA sessão
+  update: alterar alguma sessão
+  destroy (delete): deletar uma sessão
+*/
+
+import User from '../models/User'
+import * as Yup from 'yup'
+
+class SessionController {
+  async store(req, res) {
+    
+  }
+}
