c9924ef5464d7f3e31de412027bbe938b331f2c3
YURI <cg3026507@labA204m36.IFSP>
2024-06-10 08:37:30 -0300

    criando o dashboardController

---
+++ src/controllers/DashboardController.js
@@ -0,0 +1,14 @@
+import House from '../models/House';
+
+class DashboardController{
+
+  async show(req, res){
+    const { user_id } = req.headers;
+
+    const house = await House.find({ user: user_id })
+
+    return res.json(houses);
+  }
+}
+
+export default new DashboardController();
